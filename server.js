a4a20a9762970542e4411158e095f8c0685d4fc4
Chemicalz82 <chemcope82@yahoo.com>
2018-08-28 13:57:51 -0500

    completed server

--- server.js
+++ server.js
@@ -22,6 +22,14 @@ app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "home.html"));
 });
 
+app.get("/reservations", function(req, res){
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res){
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
 app.get("/api/reservations", function(req, res){
     return res.json(reservations);
 });
